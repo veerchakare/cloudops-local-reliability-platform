61b9d6eea0c428bb0cf008e942532d1d423e30f9
Veeru <189600492+veerchakare@users.noreply.github.com>
2026-09-23 23:44:12 +0000

    feat: add permission audit script

---
+++ scripts/permission_audit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "===== CloudOps Permission Audit ====="
+echo "Current User:"
+whoami
+echo
+echo "User and Group Information:"
+id
+echo
+echo "project File Permission"
+ls -la ..
